01cdff7871502cff9a662e7badc9d2aefda20432
ASergrey <114230324+ASergrey@users.noreply.github.com>
2023-02-08 15:03:02 +0300

    Changes

--- Navigation1/Controllers/FeedViewController.swift
+++ Navigation1/Controllers/FeedViewController.swift
@@ -1,32 +1,10 @@
-//
-//  FeedViewController.swift
-//  Navigation1
-//
-//  Created by  Sergey Aydarov on 25.01.2023.
-//
+
 
 import UIKit
 
 class FeedViewController: UIViewController {
 
     var post = Post(title: "Типа пост")
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemPink
-        self.view.addSubview(self.button)
-        setupButton()
-
-
-
-    }
-    private func setupButton() {
-            self.view.addSubview(self.button)
-            self.button.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -100).isActive = true
-            self.button.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20).isActive = true
-            self.button.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20).isActive = true
-            self.button.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        }
 
 
     private lazy var button: UIButton = {
@@ -41,21 +19,29 @@ class FeedViewController: UIViewController {
         return button
     }()
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+        self.view.addSubview(self.button)
+        setupButton()
+
+    }
+
+    private func setupButton() {
+            self.view.addSubview(self.button)
+            self.button.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -100).isActive = true
+            self.button.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20).isActive = true
+            self.button.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20).isActive = true
+            self.button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        }
+
+
     @objc private func buttonAction() {
            let postViewController = PostViewController()
            self.navigationController?.pushViewController(postViewController, animated: true)
         postViewController.titlePost = post.title
        }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 
--- Navigation1/Controllers/PostViewController.swift
+++ Navigation1/Controllers/PostViewController.swift
@@ -11,53 +11,20 @@ class PostViewController: UIViewController {
     var titlePost: String = "SomeOne"
 
     override func viewDidLoad() {
-
         super.viewDidLoad()
-        view.backgroundColor = .systemCyan
-        setupButton()
-
+        print("viewDidLoad")
+        self.view.backgroundColor = .systemCyan
+        let myButton = UIBarButtonItem(barButtonSystemItem: .add,
+                                       target: self,
+                                       action: #selector(goToInfo))
+        navigationItem.rightBarButtonItem = myButton
     }
-
-    // Do any additional setup after loading the view.
-    private func setupButton() {
-            self.view.addSubview(self.button)
-            self.button.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -100).isActive = true
-            self.button.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20).isActive = true
-            self.button.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20).isActive = true
-            self.button.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        }
-
-
-
-    private lazy var button: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = .purple
-        button.layer.cornerRadius = 13
-        button.setTitle("Инфо", for: .normal)
-        button.setTitleColor(.lightGray, for: .normal)
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 22)
-        button.addTarget(self, action: #selector(action), for: .touchUpInside)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-
-     @objc private func action() {
+    @objc  func goToInfo(_ sender: UIButton) {
            let infoViewController = InfoViewController()
-         self.navigationController?.pushViewController(infoViewController, animated: true)
 
+         self.navigationController?.pushViewController(infoViewController, animated: true)
        }
 
-
     }
 
-//
-    /*
-     // MARK: - Navigation
-
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
 
