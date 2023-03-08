78e512e4a798d8b325a459088dc11c7bd8c5113c
ASergrey <114230324+ASergrey@users.noreply.github.com>
2023-03-08 19:54:16 +0300

    FirstConstraintCommit

--- Navigation1/Profile/ProfileHeaderView.swift
+++ Navigation1/Profile/ProfileHeaderView.swift
@@ -10,16 +10,15 @@ import UIKit
 class ProfileHeaderView: UIView {
     lazy var screenWidth = UIScreen.main.bounds.width
 
-
     private var photoView: UIImageView =  {
         let photoView = UIImageView()
         photoView.image = #imageLiteral(resourceName: "Simba")
         photoView.layer.cornerRadius = 50
         photoView.layer.borderWidth = 3
         photoView.layer.borderColor = UIColor.white.cgColor
-        photoView.frame = CGRect(x: 16, y: 16, width: 100, height: 100)
         photoView.layer.opacity = 1
         photoView.clipsToBounds = true
+        photoView.translatesAutoresizingMaskIntoConstraints = false
         return photoView
     }()
 
@@ -29,7 +28,8 @@ class ProfileHeaderView: UIView {
         label.text = "Hipster Cat"
         label.textColor = .black
         label.font = UIFont.boldSystemFont(ofSize: 18)
-        label.frame = CGRect(x: 150, y: 27, width: 200, height: 40)
+//        label.frame = CGRect(x: 150, y: 27, width: 200, height: 40)
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
 
@@ -38,7 +38,8 @@ class ProfileHeaderView: UIView {
         label.text = "Waiting status"
         label.textColor = .gray
         label.font  = UIFont(name: "regular", size: 14)
-        label.frame = CGRect(x: 150, y: 75, width: 200, height: 40)
+//        label.frame = CGRect(x: 150, y: 75, width: 200, height: 40)
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
 
@@ -52,8 +53,9 @@ class ProfileHeaderView: UIView {
         button.layer.shadowColor = UIColor.black.cgColor
         button.layer.shadowOpacity = 0.7
         button.layer.shadowRadius = 4
-        button.frame = CGRect(x: 16, y: 160, width: (screenWidth - 32), height: 40)
+//        button.frame = CGRect(x: 16, y: 160, width: (screenWidth - 32), height: 40)
         button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
 
@@ -64,18 +66,43 @@ class ProfileHeaderView: UIView {
         textField.layer.borderColor = UIColor.black.cgColor
         textField.layer.cornerRadius = 12
         textField.placeholder = "  Write your status"
-        textField.frame = CGRect(x: 150, y: 110, width: 200, height: 40)
+//        textField.frame = CGRect(x: 150, y: 110, width: 200, height: 40)
+        textField.translatesAutoresizingMaskIntoConstraints = false
         return textField
     }()
+
 
 
     func setupLayout(){
         addSubview(photoView)
         addSubview(mainLabel)
-        addSubview(statusLabel)
         addSubview(statusTextfield)
         addSubview(statusButton)
+        addSubview(statusLabel)
+
+        NSLayoutConstraint.activate([
+            photoView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 16),
+            photoView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            photoView.heightAnchor.constraint(equalToConstant: 100),
+            photoView.widthAnchor.constraint(equalToConstant: 100),
+            mainLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 27),
+            mainLabel.leadingAnchor.constraint(equalTo: photoView.trailingAnchor, constant: 30),
+            statusButton.topAnchor.constraint(equalTo: photoView.bottomAnchor, constant: 42),
+            statusButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            statusButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            statusButton.heightAnchor.constraint(equalToConstant: 50),
+            statusLabel.leadingAnchor.constraint(equalTo: photoView.trailingAnchor, constant: 30),
+            statusLabel.bottomAnchor.constraint(equalTo: statusButton.topAnchor, constant: -64),
+            statusTextfield.leadingAnchor.constraint(equalTo: photoView.trailingAnchor, constant: 30),
+            statusTextfield.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            statusTextfield.heightAnchor.constraint(equalToConstant: 40),
+            statusTextfield.topAnchor.constraint(equalTo: statusLabel.bottomAnchor, constant: 10),
+            
+        ])
+
+
     }
+
 
     @objc func buttonAction (){
         statusLabel.text =  statusTextfield.text
--- Navigation1/Profile/ProfileViewController.swift
+++ Navigation1/Profile/ProfileViewController.swift
@@ -8,21 +8,45 @@
 import UIKit
 
 final class ProfileViewController: UIViewController {
-     let profileHeaderView = ProfileHeaderView()
+    let profileHeaderView = ProfileHeaderView()
     let witdht = UIScreen.main.bounds.width
+
+    private let newButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemGreen
+        button.layer.cornerRadius = 4
+        button.setTitle("New button", for: .normal)
+        button.layer.shadowOffset = CGSize(width: 4, height: 4)
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.7
+        button.layer.shadowRadius = 4
+        button.translatesAutoresizingMaskIntoConstraints = false
+
+        return button
+    }()
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        view.addSubview(profileHeaderView)
-
-    }
-
-    override func viewWillLayoutSubviews(){
-        super.viewWillLayoutSubviews()
-        profileHeaderView.backgroundColor = .lightGray
-        profileHeaderView.frame = self.view.frame
-        profileHeaderView.frame = CGRect(x: 0, y: 100, width: witdht, height: 750)
+        layaut()
         profileHeaderView.setupLayout()
     }
 
+
+    func layaut() {
+        profileHeaderView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(profileHeaderView)
+        view.addSubview(newButton)
+        profileHeaderView.backgroundColor = .lightGray
+        NSLayoutConstraint.activate([
+        profileHeaderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+        profileHeaderView.leadingAnchor.constraint(equalTo: view.leadingAnchor , constant: 0),
+        profileHeaderView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+        profileHeaderView.heightAnchor.constraint(equalToConstant: 220),
+        newButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+        newButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+        newButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0)
+        ])
+    }
 }
